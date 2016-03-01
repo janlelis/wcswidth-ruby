c11b1bddccfc97a8e41f102c9b27c3fa8efa2f07
Jan Lelis <mail@janlelis.de>
2016-03-01 18:12:49 +0100

    Check with defined? if constants exist, release 1.0

--- lib/wcswidth/sizes.rb
+++ lib/wcswidth/sizes.rb
@@ -4,18 +4,18 @@ end
 begin
   require "rbconfig/sizeof"
 
-  Wcswidth::MAX_WIDTH = 256 ** RbConfig::SIZEOF["size_t"] - 1
+  Wcswidth::MAX_WIDTH  = 256 ** RbConfig::SIZEOF["size_t"] - 1
   Wcswidth::WCHAR_SIZE = RbConfig::SIZEOF["wchar_t"]
 rescue LoadError
 end
 
 require "rbconfig"
 
-if !Wcswidth::MAX_WIDTH
+unless defined?(Wcswidth::MAX_WIDTH) && Wcswidth::MAX_WIDTH
   Wcswidth::MAX_WIDTH = 18446744073709551615
 end
 
-if !Wcswidth::WCHAR_SIZE
+unless defined?(Wcswidth::WCHAR_SIZE) && Wcswidth::WCHAR_SIZE
   if RbConfig::CONFIG["host_os"] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/
     Wcswidth::WCHAR_SIZE = 2
   else
